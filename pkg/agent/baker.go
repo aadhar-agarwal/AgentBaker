5e3aab840c102f1ec7a6d459fe4aecd2b23c1147
Aadhar Agarwal <aadagarwal@microsoft.com>
2026-04-08 21:38:44 +0000

    feat: add OSSKU-based IsCgroupV2 detection and CustomizedImageTrustedLaunch distro

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -621,7 +621,9 @@ func getContainerServiceFuncMap(config *datamodel.NodeBootstrappingConfiguration
 			return config.GetOrderedKubeproxyConfigStringForPowershell()
 		},
 		"IsCgroupV2": func() bool {
-			return profile.Is2204VHDDistro() || profile.IsAzureLinuxCgroupV2VHDDistro() || profile.Is2404VHDDistro() || profile.IsFlatcar() || profile.IsACL()
+			return profile.Is2204VHDDistro() || profile.IsAzureLinuxCgroupV2VHDDistro() ||
+				config.OSSKU == datamodel.OSSKUAzureLinux || profile.Is2404VHDDistro() ||
+				config.IsFlatcar() || config.IsACL()
 		},
 		"GetKubeProxyFeatureGatesPsh": func() string {
 			return cs.Properties.GetKubeProxyFeatureGatesWindowsArguments()
@@ -717,7 +719,8 @@ func getContainerServiceFuncMap(config *datamodel.NodeBootstrappingConfiguration
 		"IsCustomImage": func() bool {
 			return profile.Distro == datamodel.CustomizedImage ||
 				profile.Distro == datamodel.CustomizedImageKata ||
-				profile.Distro == datamodel.CustomizedImageLinuxGuard
+				profile.Distro == datamodel.CustomizedImageLinuxGuard ||
+				profile.Distro == datamodel.CustomizedImageTrustedLaunch
 		},
 		"EnableHostsConfigAgent": func() bool {
 			return cs.Properties.OrchestratorProfile.KubernetesConfig != nil &&
